9feeec1395823b20eb77ec2aaea8d8279d5b0fc7
daniel-dataflow <daniel.dataflow@gmail.com>
2025-09-22 17:42:18 +0900

    20250922

--- multi01_python/07_class/class10.py
+++ multi01_python/07_class/class10.py
@@ -1,6 +1,6 @@
 class Parent:
     def __new__(cls, *args, **kwargs):
-        return super().__new__(cls, *arg, **kwargs)
+        return super().__new__(cls, *args, **kwargs)
 
     def __init__(self):
         print("Parent init")
@@ -16,7 +16,7 @@ class Parent:
 class Child(Parent):
 
     def __new__(cls, *args, **kwargs):
-        return super().__new__(cls, *arg, **kwargs)
+        return super().__new__(cls, *args, **kwargs)
 
     def __init__(self):
         super().__init__() # 이 구문을 통해서 부모가 가지고 있는 변수 초기화
@@ -26,7 +26,7 @@ class Child(Parent):
 if __name__ == "__main__":
     child = Child() # Child()라고 쓰면 객체화 되고-> child init 호출 --> child new 호출  -> parent init 호출 -> parent new 호출
                     # -> Object -> 리턴으로 부모객체 만들어짐 -> 리턴으로 자식객체가 만들어짐 -> 인스턴스 함수를 초기화
-                    # -> super-> parent init 호출 프린트-> child init -> 출력
+                    # -> super().__init__() -> parent init 호출 프린트-> child init 호출 프린터
     child.prn01()
     child.prn02()
 
