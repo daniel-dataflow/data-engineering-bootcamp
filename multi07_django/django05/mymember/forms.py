96299302e71912bd14b844a32a65e71e3b4d5d05
daniel-dataflow <daniel.dataflow@gmail.com>
2026-01-21 14:19:37 +0900

    20260121

---
+++ multi07_django/django05/mymember/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class MyMemberForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username','password1', 'password2', 'first_name', 'last_name', 'email')
+
+        """
+        UserCreationForm 이 가진 기본적인 필드
+        username
+        password1 : 비밀번호
+        password2 : 비밀번호 확인
+        """
---
+++ multi07_django/django06/updown/views.py
@@ -0,0 +1,22 @@
+from http.client import responses
+
+from django.shortcuts import render
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def upload_process(request):
+    upload_file = request.FILES['uploadfile']
+
+    uploaded_name = default_storage.save(upload_file.name, ContentFile(upload_file.read()))
+    return render(request, 'download.html', {'filename': uploaded_name})
+
+def download_process(request, filename):
+    responses = HttpResponse(default_storage.open(filename).read(), content_type='application/force-download')
+
+    return responses
+
+
